375bd844642233b5d595f49fc87299d53d13fce1
wowbob396 <wowbob396@gmail.com>
2017-06-22 21:05:00 -0500

    added a todo

--- testscript.py
+++ testscript.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
-my_url = 'https://99bitcoins.com/price-chart-history/'
+my_url = 'https://www.tradingview.com/chart/?symbol=BITSTAMP:BTCUSD'
  #read data from url
 uClient = uReq(my_url)
 page_html = uClient.read()
@@ -10,3 +10,4 @@ uClient.close()
 pageSoup = soup(page_html,"html.parser")
 #grab videogame container
 containers = pageSoup.findAll("div",{"class" : "s-item-container"})
+pageSoup.div
